474d5abeacf5490ad60e552cc2e29cafbf597037
lzbgithubcode <1835064412@qq.com>
2016-11-01 11:11:27 +0800

    init

---
+++ LZBTabbarViewController/LZBTabbarViewController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LZBTabbarViewController
+//
+//  Created by zibin on 16/11/1.
+//  Copyright © 2016年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
